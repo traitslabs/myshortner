3eb4a872bd67e79dc55b60831160c12256cd8132
traitslabs <124496959+traitslabs@users.noreply.github.com>
2025-11-13 03:37:54 +0500

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,1 +1,1 @@
-<?php session_start(); session_destroy(); header("Location: index.php"); ?>
+<?php header('Content-Type: text/html; charset=utf-8'); session_start(); session_destroy(); header("Location: index.php"); ?>
